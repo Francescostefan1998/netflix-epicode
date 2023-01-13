d8dc4dbcf664e2b42b5ec333f95f15b0e2c5a39b
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-01-13 17:42:15 +0100

    fixing cors problems

--- src/components/RecentlyAdded.jsx
+++ src/components/RecentlyAdded.jsx
@@ -1,52 +1,31 @@
-import { Component } from "react";
 import SingleMovie from "./SingleMovie";
-class RecentlyAdded extends Component {
-  state = {
-    movie: [],
-  };
-
-  /*fetchMovie = async () => {
-    try {
-      let response = await fetch(
-        "https://benchmark-exam-2-week-backend-production.up.railway.app/medias"
-      );
-      if (response.ok) {
-        let data = await response.json();
-        this.setState({
-          movie: data,
-        });
-        console.log("fetched!");
-        console.log(data);
-        console.log(this.state.movie);
-        console.log(this.state.movie);
-      } else {
-        console.log("error fetching");
-        alert("movie not found");
-      }
-    } catch (error) {
-      console.log("error");
-    }
-  };
-*/
-  componentDidMount() {
-    console.log("component did mount");
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { useEffect } from "react";
+import { useState } from "react";
+const RecentlyAdded = (props) => {
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  console.log(products[0]);
+  useEffect(() => {
+    console.log("useeffect triggered");
     async function fetchProducts() {
       const apiUrl = process.env.REACT_APP_BE;
       const resp = await fetch(`${apiUrl}/medias`);
       const data = await resp.json();
-      this.setState({ movie: data });
+      setProducts(data);
     }
-  }
-  render() {
-    return (
-      <div>
-        <h1>Added from my API</h1>
-        {this.state.movie.map((movie, i) => (
-          <SingleMovie movie={movie} key={i} />
-        ))}
-      </div>
-    );
-  }
-}
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Movie fetched by me</h1>
+      {products.map((movie, i) => (
+        <SingleMovie movie={movie} key={i} />
+      ))}
+    </div>
+  );
+};
 
 export default RecentlyAdded;
